2e6fac1ce930f95e59a7ac8f899c453adcd2d451
Sahil <mohammadsahil23786@gmail.com>
2022-04-04 18:11:22 +0530

    added params in browser

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
         // this automated browsing experience)
         let launchOptions = { headless: false, args: ['--start-maximized'] };
 
-        const browser = await puppeteer.launch(launchOptions);
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
 
         // set viewport and user agent (just in case for nice viewing)
